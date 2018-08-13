71a5a083e32b31f5a793a8f5ed92a3d8bf8457f8
Stephane Lafontaine <stephane@medqigong.ca>
2018-08-13 17:50:10 -0400

    Set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :caves do
-    
+  # resources :users
+
+  resources :caves do 
+    resources :bookings, only: [ :new, :create]
   end
-  resources :users
-  resources :bookings
+  resources :bookings, except: [ :new, :create]
 
 end

aaff6f377a66e32da43e7856770caba73f1b5fc2
Veratio 800 <geeemdee@protonmail.ch>
2018-08-14 11:39:26 -0400

    Added description column to caves table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_13_205846) do
+ActiveRecord::Schema.define(version: 2018_08_14_153216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2018_08_13_205846) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.text "description"
     t.index ["user_id"], name: "index_caves_on_user_id"
   end
 
@@ -58,7 +59,7 @@ ActiveRecord::Schema.define(version: 2018_08_13_205846) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "bookings", "caves", column: "cave_id"
+  add_foreign_key "bookings", "caves"
   add_foreign_key "bookings", "users"
   add_foreign_key "caves", "users"
 end

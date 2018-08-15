05a5a66803f4bfeac37cc218b665c5055cef6447
Justin Bernold <jbernold@gmail.com>
2018-08-15 11:40:55 -0400

    updated seed file to include bookings, cave addresses, and cave_amenity pairings. tweaked cave model to destroy dependent cave_amenities so db can still be cleaned during seed. TODO: add caveman photo, first name, and last name once db is updated.

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,7 @@ Booking.destroy_all
 Cave.destroy_all
 Amenity.destroy_all
 User.destroy_all
+CaveAmenity.destroy_all
 puts "Seeding database..."
 
 User.create(email: "captain@caveman.com", password: "123456")
@@ -26,6 +27,8 @@ User.create(email: "conan@thebarbarian.com", password: "123456")
 User.create(email: "gollum@thepreciouss.net", password: "123456")
 User.create(email: "barney@rubble.com", password: "123456")
 User.create(email: "fred@flintstone.com", password: "123456")
+User.create(email: "balrog@moria.com", password: "123456")
+User.create(email: "smaug@lonelymountain.com", password: "123456")
 
 Amenity.create(name:"Boulder", description:"Roll some sweet hard rock goodness in front of your cave to seal the entrance against the elements (and dinosaurs)", icon:'<i class="fas fa-meh-rolling-eyes"></i>')
 Amenity.create(name:"Covered Pit", description:"Experience the sweet relief of aromatic freedom, as you close the lid on your, um, excretions.", icon:'<i class="fa fa-poo"></i>')
@@ -38,11 +41,57 @@ Amenity.create(name:"Flowing Water", description:"This cave is close to a water 
 Amenity.create(name:"Forbidden Fruit", description:"Get some tasty knowledge and vitamin C with this cave's stash of forbidden fruit.", icon:'<i class="fab fa-apple"></i>')
 Amenity.create(name:"Air Freshener", description:"This cave has an air freshener.", icon:'fas fa-air-freshener')
 
-Cave.create(name: "Gollum's Preciouss Pad", user: User.third, description: "It's so precious!", price: "4", remote_photo_url: 'https://wonderopolis.org/_img?img=/wp-content/uploads/2013/10/dreamstime_xl_4842587-custom.jpg&transform=resizeCrop,720,450')
-Cave.create(name: "Captain's Unga Bungalow", user: User.first, description: "Unga bunga!", price: "6", remote_photo_url: 'https://img-aws.ehowcdn.com/877x500p/photos.demandstudios.com/getty/article/144/177/177044797.jpg')
-Cave.create(name: "Cimmerian Spice", user: User.second, description: "What is best in life? To crush your enemies, to see them driven before you, and to hear the lamentations of whoever you beat to this cave.", price: "5", remote_photo_url: 'https://www.asiatouradvisor.com/wp-content/uploads/2017/11/8-most-attractive-caves-visitors-can-explore-in-Quang-Binh4.jpg')
+Cave.create(name: "Gollum's Preciouss Pad", user: User.third, description: "It's so precious!", price: "4", remote_photo_url: 'https://i.ytimg.com/vi/jcUEaQLZA2c/maxresdefault.jpg', address: "5333 av Casgrain, Montreal")
+Cave.create(name: "Captain's Unga Bungalow", user: User.first, description: "Unga bunga!", price: "6", remote_photo_url: 'https://img-aws.ehowcdn.com/877x500p/photos.demandstudios.com/getty/article/144/177/177044797.jpg', address: "4692 boul Saint Laurent, Montreal")
+Cave.create(name: "Cimmerian Spice", user: User.second, description: "What is best in life? To crush your enemies, to see them driven before you, and to hear the lamentations of whoever you beat to this cave.", price: "5", remote_photo_url: 'https://www.asiatouradvisor.com/wp-content/uploads/2017/11/8-most-attractive-caves-visitors-can-explore-in-Quang-Binh4.jpg', address: "82 rue villeneuve, Montreal")
+Cave.create(name: "Smaug's Lair", user: User.all[6], description: "My armour is like tenfold shields, my teeth are swords, my claws spears, the shock of my tail is a thunderbolt, my wings a hurricane, and my breath death! But despite all that, my cave is very welcoming. Especially if you're into gold and mithril and weapons and stuff.", price: "15", remote_photo_url: 'https://blenderartists.org/uploads/default/original/4X/d/0/f/d0fddfdb03c74e9d43098418ca4f0fc3f0d6630d.jpg', address: "74 Avenue Fairmount O, Montreal")
+Cave.create(name: "Barney's Bamm-Bamm", user: User.fourth, description: "This is the best cave ever!", price: "12", remote_photo_url: 'https://wonderopolis.org/_img?img=/wp-content/uploads/2013/10/dreamstime_xl_4842587-custom.jpg&transform=resizeCrop,720,450', address: "263 Rue Saint Viateur O, Montreal")
+Cave.create(name: "Moria Mine All Mine", user: User.all[5], description: "You shall not pass . . . on this awesome opportunity!", price: "10", remote_photo_url: 'https://static.giantbomb.com/uploads/original/1/17172/1405201-lotr_38.jpg', address: "7030 Boulevard Saint-Michel, Montreal")
+Cave.create(name: "Pebbles Place", user: User.fifth, description: "It's a yabba dabba do time for the whole family.", price: "3", remote_photo_url: 'http://johnkstuff.blogspot.com/uploaded_images/FFbg01house-760911.jpg', address: "267 Rue Saint Zotique O, Montreal")
 
-Booking.create(cave: Cave.first, user: User.first, checkin: "10-10-2018", checkout: "10-12-2018")
-Booking.create(cave: Cave.last, user: User.first, checkin: "10-10-2018", checkout: "10-12-2018")
-Booking.create(cave: Cave.second, user: User.second, checkin: "10-11-2018", checkout: "11-12-2018")
+Booking.create(cave: Cave.first, user: User.first, checkin: "10-10-2018", checkout: "10-12-2018", status: "proposed")
+Booking.create(cave: Cave.second, user: User.second, checkin: "10-10-2018", checkout: "10-12-2018", status: "proposed")
+Booking.create(cave: Cave.third, user: User.third, checkin: "10-11-2018", checkout: "11-12-2018", status: "proposed")
+Booking.create(cave: Cave.fourth, user: User.fourth, checkin: "10-10-2018", checkout: "10-12-2018", status: "proposed")
+Booking.create(cave: Cave.fifth, user: User.fifth, checkin: "10-10-2018", checkout: "10-12-2018", status: "proposed")
+Booking.create(cave: Cave.all[5], user: User.all[5], checkin: "10-11-2018", checkout: "11-12-2018", status: "proposed")
+Booking.create(cave: Cave.all[6], user: User.all[6], checkin: "10-11-2018", checkout: "11-12-2018", status: "proposed")
+
+Booking.create(cave: Cave.first, user: User.second, checkin: "10-11-2018", checkout: "10-12-2018", status: "accepted")
+Booking.create(cave: Cave.second, user: User.third, checkin: "10-10-2018", checkout: "10-12-2018", status: "accepted")
+Booking.create(cave: Cave.third, user: User.fourth, checkin: "10-11-2018", checkout: "11-12-2018", status: "accepted")
+Booking.create(cave: Cave.fourth, user: User.fifth, checkin: "10-10-2018", checkout: "10-12-2018", status: "accepted")
+Booking.create(cave: Cave.fifth, user: User.all[5], checkin: "10-10-2018", checkout: "10-12-2018", status: "accepted")
+Booking.create(cave: Cave.all[5], user: User.all[6], checkin: "10-11-2018", checkout: "11-12-2018", status: "accepted")
+Booking.create(cave: Cave.all[6], user: User.first, checkin: "10-11-2018", checkout: "11-12-2018", status: "accepted")
+
+Booking.create(cave: Cave.first, user: User.third, checkin: "10-11-2018", checkout: "10-12-2018", status: "rejected")
+Booking.create(cave: Cave.second, user: User.fourth, checkin: "10-10-2018", checkout: "10-12-2018", status: "rejected")
+Booking.create(cave: Cave.third, user: User.fifth, checkin: "10-11-2018", checkout: "11-12-2018", status: "rejected")
+Booking.create(cave: Cave.fourth, user: User.all[5], checkin: "10-10-2018", checkout: "10-12-2018", status: "rejected")
+Booking.create(cave: Cave.fifth, user: User.all[6], checkin: "10-10-2018", checkout: "10-12-2018", status: "rejected")
+Booking.create(cave: Cave.all[5], user: User.first, checkin: "10-11-2018", checkout: "11-12-2018", status: "rejected")
+Booking.create(cave: Cave.all[6], user: User.second, checkin: "10-11-2018", checkout: "11-12-2018", status: "rejected")
+
+Booking.create(cave: Cave.first, user: User.fourth, checkin: "10-11-2018", checkout: "10-12-2018", status: "cancelled")
+Booking.create(cave: Cave.second, user: User.fifth, checkin: "10-10-2018", checkout: "10-12-2018", status: "cancelled")
+Booking.create(cave: Cave.third, user: User.all[5], checkin: "10-11-2018", checkout: "11-12-2018", status: "cancelled")
+Booking.create(cave: Cave.fourth, user: User.all[6], checkin: "10-10-2018", checkout: "10-12-2018", status: "cancelled")
+Booking.create(cave: Cave.fifth, user: User.first, checkin: "10-10-2018", checkout: "10-12-2018", status: "cancelled")
+Booking.create(cave: Cave.all[5], user: User.second, checkin: "10-11-2018", checkout: "11-12-2018", status: "cancelled")
+Booking.create(cave: Cave.all[6], user: User.third, checkin: "10-11-2018", checkout: "11-12-2018", status: "cancelled")
+
+Booking.create(cave: Cave.first, user: User.fifth, checkin: "10-11-2018", checkout: "10-12-2018", status: "cancelrequested")
+Booking.create(cave: Cave.second, user: User.all[5], checkin: "10-10-2018", checkout: "10-12-2018", status: "cancelrequested")
+Booking.create(cave: Cave.third, user: User.all[6], checkin: "10-11-2018", checkout: "11-12-2018", status: "cancelrequested")
+Booking.create(cave: Cave.fourth, user: User.first, checkin: "10-10-2018", checkout: "10-12-2018", status: "cancelrequested")
+Booking.create(cave: Cave.fifth, user: User.second, checkin: "10-10-2018", checkout: "10-12-2018", status: "cancelrequested")
+Booking.create(cave: Cave.all[5], user: User.third, checkin: "10-11-2018", checkout: "11-12-2018", status: "cancelrequested")
+Booking.create(cave: Cave.all[6], user: User.fourth, checkin: "10-11-2018", checkout: "11-12-2018", status: "cancelrequested")
+
+(0..9).each do |n|
+   (0..6).to_a.shuffle.take(rand(7)).each do |x|
+CaveAmenity.create(cave: Cave.all[n], amenity: Amenity.all[x])
+  end
+end
 #

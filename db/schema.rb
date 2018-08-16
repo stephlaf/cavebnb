e89fa651957d1e6e2513f3ec0235ce7a1ceb9de5
Stephane Lafontaine <stephane@medqigong.ca>
2018-08-16 17:55:00 -0400

    Fixed alert's display and style which were messing up the navbar; fixed search field so ther's no autocomplete; added z-index to navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_08_16_162144) do
+ActiveRecord::Schema.define(version: 2018_08_16_175625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,15 +76,6 @@ ActiveRecord::Schema.define(version: 2018_08_16_162144) do
     t.index ["user_id"], name: "index_caves_on_user_id"
   end
 
-
-  create_table "pg_search_documents", force: :cascade do |t|
-    t.text "content"
-    t.string "searchable_type"
-    t.bigint "searchable_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
-
   create_table "conversations", force: :cascade do |t|
     t.integer "sender_id"
     t.integer "recipient_id"
@@ -101,6 +91,15 @@ ActiveRecord::Schema.define(version: 2018_08_16_162144) do
     t.datetime "updated_at", null: false
     t.index ["conversation_id"], name: "index_messages_on_conversation_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
   end
 
   create_table "reviews", force: :cascade do |t|

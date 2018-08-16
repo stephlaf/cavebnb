1fef2d064718b17f8b79ce5af58ce14a07705a99
Everett Webre <evwebre@gmail.com>
2018-08-16 11:32:04 -0400

    Revert "Merge pull request #48 from stephlaf/user_devise_views"
    This reverts commit 37af2b09a593213c51d0924290a113c9bf08a088, reversing
    changes made to 561e1c0bc754b29f842adfebf90092d1406e18f0.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  mount_uploader :photo, PhotoUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 

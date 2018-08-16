e9369485382918f6cb6c54d7a8123ad446c23f1d
Everett Webre <evwebre@gmail.com>
2018-08-16 17:27:50 -0400

    skeleton profile page

--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,13 @@ Rails.application.routes.draw do
     resources :bookings, only: [ :new, :create]
   end
 
-<<<<<<< HEAD
+
   resources :bookings, except: [ :new, :create]
   resources :users, only: [:show]
-=======
+
   resources :conversations, only: [:index, :new, :create] do
     resources :messages, only: [:index, :create]
   end
 
->>>>>>> master
+
 end
